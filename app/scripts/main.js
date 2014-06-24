798e8a2f32e7f7e31f36cf8bb060b4885ec34829
karlos g liberal <karlosgliberal@gmail.com>
2014-06-24 17:05:38 +0200

    Pruebas con el iscroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,7 @@
-$(document).ready(function(){
-    var myScroll = new IScroll('#wrapper', { mouseWheel: true, click: true });
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-});
+var myScroll;
+
+function loaded () {
+  myScroll = new IScroll('#wrapper', { mouseWheel: true, click: true });
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
