7072d3dfc05db52535d20649ed8382e34088944c
Aitor Resano <aitor.rl@gmail.com>
2014-06-24 17:06:06 +0200

    Js de Karlos

--- app/scripts/main.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-    var myScroll = new IScroll('#wrapper', { mouseWheel: true, click: true });
-    document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-});
