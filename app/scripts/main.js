ec26619aaac79262e473c9b7e33e6355ec99cde3
Aitor Resano <aitor.rl@gmail.com>
2014-06-25 18:25:39 +0200

    ordennnnnnnnnn

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $('.home').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage'],
+    resize: false,
     slidesColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
     // css3: true ,
     slidesNavigation: true,
