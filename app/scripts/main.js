201a35e52603c4cfd820210cf38254018356ec0d
karlos g liberal <karlosgliberal@gmail.com>
2014-06-25 13:04:47 +0200

    Sistema de fullpage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-  $('#fullpage').fullpage({
+
+  $('.home').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage'],
     slidesColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
     css3: true ,
-    slidesNavigation: true,     
+    slidesNavigation: true,
+    afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
+      if(anchorLink == '3rdPage'){
+      }
+    }   
   });
 
-  $('#movida').click(function(e){
-    console.log('hola');
-    e.preventDefault();
-    $.fn.fullpage.moveSlideLeft();
+  $('#boton').click(function(e){
+    $('.menus').addClass('transicion');
+    $.fn.fullpage.moveTo('3rdPage', 1); 
   });
+
 });
+
+  // $('#segunda').fullpage({
+  //   anchors: ['dosuno', 'dosdos', 'dostres'],
+  //   slidesColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+  //   css3: true ,
+  //   slidesNavigation: true,
+  //   autoScrolling: false     
+  // });
+
+
+
