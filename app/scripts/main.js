999ce0553a54504642691a023f62245201a06e68
Aitor Resano <aitor.rl@gmail.com>
2014-06-25 17:44:03 +0200

    comittt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('.home').fullpage({
     anchors: ['firstPage', 'secondPage', '3rdPage'],
     slidesColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-    css3: true ,
+    // css3: true ,
     slidesNavigation: true,
     afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex){
       if(anchorLink == '3rdPage'){
