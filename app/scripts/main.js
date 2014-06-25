cbf9c6a93a225d828127f9b5ddeb94800f985e31
karlos g liberal <karlosgliberal@gmail.com>
2014-06-25 10:02:27 +0200

    Probando fullpages.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,14 @@
-var myScroll;
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', '3rdPage'],
+    slidesColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    css3: true ,
+    slidesNavigation: true,     
+  });
 
-function loaded () {
-  myScroll = new IScroll('#wrapper', { mouseWheel: true, click: true });
-}
-
-document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+  $('#movida').click(function(e){
+    console.log('hola');
+    e.preventDefault();
+    $.fn.fullpage.moveSlideLeft();
+  });
+});
